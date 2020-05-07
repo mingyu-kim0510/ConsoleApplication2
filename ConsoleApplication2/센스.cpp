5f56d17b9cbfcf386a2e44c9f5d7913ab474fe9e
민규 김 <mgmg0510@outlook.kr>
2020-05-07 18:19:55 +0900

    tyuasdfsdaf

---
+++ ConsoleApplication2/센스.cpp
@@ -0,0 +1,7 @@
+/* 첫번째 프로그램*/
+#include <"stdio.h">
+
+void main()
+{
+	printf("hellow~ world~");
+}
---
+++ ConsoleApplication2/소스.cpp
@@ -0,0 +1,9 @@
+/* 첫번째 프로그램*/
+#include <stdio.h>
+
+int main()
+{
+	printf("hellow~ world~");
+
+	return 0:
+}
